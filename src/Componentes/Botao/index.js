005f410334dba5afe03d5b9b37b63f727431b769
EnzoMbz <103067639+EnzoMbz@users.noreply.github.com>
2023-07-18 13:05:31 -0300

    alterando cores e finalizando projeto

--- src/Componentes/Botao/index.js
+++ src/Componentes/Botao/index.js
@@ -2,13 +2,10 @@ import './Botao.css'
 
 const Botao = (props) => {
 
-    var teste = new Audio();
-    teste.src = '/audio/botao-audio.mp3'
-
     const conteudo = `${props.children}`
 
     return (
-        <button onMouseOver={e => teste.play()} className='botao'>
+        <button className='botao'>
             {conteudo}
         </button>
     )
